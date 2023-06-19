4e58cd9d64ff8a3245db3bdf346b381b9e6c1218
IKA3RUS <w@abhishekrein.xyz>
2023-06-20 05:02:03 +0530

    📱 Support smaller breakpoints

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Icon } from '@iconify/react';
 import { Popover, Transition } from '@headlessui/react';
 
 // import { Root } from 'postcss';
@@ -22,14 +23,133 @@ export default function RootLayout({ children }) {
             <body
                 className={`${degularDisplay.variable} ${ttHoves.variable} bg-white`}
             >
-                <header className="container mx-auto mt-10 flex items-start justify-between px-5">
+                <header className="container mx-auto mt-5 flex items-start justify-between px-5 md:mt-10">
                     <Image
                         src={indiaHciLogo}
                         alt="India HCI 2023 Logo"
                         width={140}
                         height={117}
                     />
-                    <nav className="flex gap-14 ">
+
+                    <nav className="gap-14 md:hidden">
+                        <Popover className="">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button
+                                        className={`${
+                                            open
+                                                ? 'text-amber-500'
+                                                : 'text-zinc-900'
+                                        }
+                                        inline-flex items-center text-sm font-semibold uppercase outline-none`}
+                                    >
+                                        <Icon
+                                            className="h-8 w-8 text-zinc-900"
+                                            icon="solar:hamburger-menu-outline"
+                                        />
+                                    </Popover.Button>
+                                    <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1"
+                                    >
+                                        <Popover.Panel className="absolute left-[20px] top-2 z-30 mt-3 flex h-[calc(100%-132px)] w-[calc(100%-40px)] flex-col overflow-scroll rounded-2xl bg-zinc-900 font-body text-sm font-normal text-white outline-none outline-1 outline-offset-0 outline-zinc-500">
+                                            <Popover.Button className="absolute right-4 top-4">
+                                                <Icon
+                                                    className="h-8 w-8 text-white"
+                                                    icon="solar:close-square-bold"
+                                                />
+                                            </Popover.Button>
+                                            <div className="flex flex-col gap-4 border-b border-zinc-800 p-6">
+                                                <p className="inline-flex items-center text-sm font-semibold uppercase text-amber-500">
+                                                    Conference
+                                                </p>
+                                                <Link className="py-2" href="/">
+                                                    Organizing Committee
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Student Volunteers
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Partners & Sponsors
+                                                </Link>
+                                            </div>
+                                            <div className="flex flex-col gap-4 p-6">
+                                                <p className="inline-flex items-center text-sm font-semibold uppercase text-amber-500">
+                                                    Attendees
+                                                </p>
+                                                <Link className="py-2" href="/">
+                                                    Conference Program
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Keynote Speakers
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Invited Speakers
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Accepted Presentations
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Featured Courses
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Featured Workshops
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Featured Courses
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Instructions To Attendees
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Registrations
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Session Recording Policy
+                                                </Link>
+                                            </div>
+                                            <div className="flex flex-col gap-4 border-b border-zinc-800 p-6">
+                                                <p className="inline-flex items-center text-sm font-semibold uppercase text-amber-500">
+                                                    Authors
+                                                </p>
+                                                <Link className="py-2" href="/">
+                                                    Full Paper
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Student Research Consortium
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Student Design Consortium
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Posters & Demos
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Industry & Startups
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Courses
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Workshops
+                                                </Link>
+                                                <Link className="py-2" href="/">
+                                                    Out of India
+                                                </Link>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+                    </nav>
+                    <nav className="hidden gap-14 md:flex ">
                         <Popover className="relative">
                             {({ open }) => (
                                 <>
@@ -69,7 +189,43 @@ export default function RootLayout({ children }) {
                                                 className="border-b border-zinc-800 p-4"
                                                 href="/"
                                             >
+                                                Student Volunteers
+                                            </Link>
+                                            <Link
+                                                className="border-b border-zinc-800 p-4"
+                                                href="/"
+                                            >
                                                 Partners & Sponsors
+                                            </Link>
+                                            <Link
+                                                className="border-b border-zinc-800 p-4"
+                                                href="/"
+                                            >
+                                                Student Volunteers
+                                            </Link>
+                                            <Link
+                                                className="border-b border-zinc-800 p-4"
+                                                href="/"
+                                            >
+                                                Student Volunteers
+                                            </Link>
+                                            <Link
+                                                className="border-b border-zinc-800 p-4"
+                                                href="/"
+                                            >
+                                                Student Volunteers
+                                            </Link>
+                                            <Link
+                                                className="border-b border-zinc-800 p-4"
+                                                href="/"
+                                            >
+                                                Student Volunteers
+                                            </Link>
+                                            <Link
+                                                className="border-b border-zinc-800 p-4"
+                                                href="/"
+                                            >
+                                                Student Volunteers
                                             </Link>
                                         </Popover.Panel>
                                     </Transition>
@@ -244,7 +400,7 @@ export default function RootLayout({ children }) {
                 </header>
                 {children}
                 <div className="bg-zinc-900">
-                    <footer className="container mx-auto mt-10 flex items-start justify-between p-20 px-5">
+                    <footer className="container mx-auto flex flex-col items-center justify-between gap-40 p-12 px-5 sm:flex-row sm:gap-0">
                         <a
                             href="https://www.indiahci.org/2023/"
                             target="_blank"
@@ -253,11 +409,10 @@ export default function RootLayout({ children }) {
                             <Image
                                 src={indiaHciLogoMonochrome}
                                 alt="India HCI 2023 Logo"
-                                width={80}
-                                height={48}
+                                width={120}
                             />
                         </a>
-                        <div className="flex flex-row items-center gap-20">
+                        <div className="relative right-4 flex flex-col items-center gap-10 sm:flex-row sm:gap-20">
                             <a
                                 href="https://www.indiahci.org/"
                                 target="_blank"
--- app/page.jsx
+++ app/page.jsx
@@ -13,14 +13,14 @@ export default function Home() {
     return (
         <div className="flex flex-col">
             <main className="container mx-auto mt-14 flex flex-col px-5">
-                <div className="mt-0 flex grow flex-row items-center justify-between">
-                    <div className="flex flex-row gap-24">
+                <div className="flex grow flex-col items-start justify-between gap-10 lg:flex-row lg:items-center lg:gap-0">
+                    <div className="flex flex-col gap-10 lg:flex-row lg:gap-24">
                         <div className="flex flex-col gap-6 p-0">
                             <Icon
                                 className="h-6 w-6 text-amber-500"
                                 icon="solar:calendar-add-bold"
                             />
-                            <div className="font-body text-lg font-light">
+                            <div className="font-body text-base font-light lg:text-lg">
                                 23-24 November 2023
                             </div>
                         </div>
@@ -29,25 +29,25 @@ export default function Home() {
                                 className="h-6 w-6 text-amber-500"
                                 icon="solar:map-point-favourite-bold"
                             />
-                            <div className="font-body text-lg font-light">
+                            <div className="font-body text-base font-light lg:text-lg">
                                 School of Design, UPES, Dehradun
                             </div>
                         </div>
                     </div>
-                    <Icon className="hidden md:block" icon="octicon:x-12" />
+                    <Icon className="hidden lg:block" icon="octicon:x-12" />
                     <div className="flex flex-col gap-6 p-0 font-body text-lg">
                         <Icon
                             className="h-6 w-6 text-amber-500"
                             icon="solar:letter-bold"
                         />
-                        <div className="font-body text-lg font-light">
+                        <div className="font-body text-base font-light lg:text-lg">
                             info@indiahci.org
                         </div>
                     </div>
                 </div>
-                <div className="mx-5 mt-28 flex flex-row gap-6">
-                    <div className="relative -z-20 flex max-w-lg flex-col gap-10 rounded-lg rounded-bl-none rounded-br-none bg-zinc-50 bg-gradient-to-b px-8 pb-24 pt-10 outline outline-1 outline-zinc-100">
-                        <h1 className="bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-4xl text-transparent">
+                <div className="mt-14 flex flex-col gap-6 lg:mx-5 lg:mt-28 lg:flex-row">
+                    <div className="relative -z-20 flex w-full flex-col gap-8 rounded-lg bg-zinc-50 bg-gradient-to-b p-5 outline outline-1 outline-zinc-100 sm:gap-10 sm:px-8 sm:pb-8 sm:pt-10 lg:max-w-lg lg:pb-24">
+                        <h1 className="bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-2xl text-transparent sm:text-4xl">
                             Welcome to India HCI
                         </h1>
                         <div className="font-body text-[16] opacity-50">
@@ -61,8 +61,8 @@ export default function Home() {
                             lush and verdant environs.
                         </div>
                     </div>
-                    <div className="relative -z-20 flex max-w-lg flex-col gap-10 rounded-lg rounded-bl-none rounded-br-none bg-zinc-50 bg-gradient-to-b px-8 pb-24 pt-10 outline outline-2 outline-zinc-200">
-                        <h1 className="relative bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-4xl text-transparent">
+                    <div className="relative -z-20 flex w-full flex-col gap-8 rounded-lg bg-zinc-50 bg-gradient-to-b p-5 outline outline-1 outline-zinc-100 sm:gap-10 sm:px-8 sm:pb-8 sm:pt-10 lg:max-w-lg lg:pb-24">
+                        <h1 className="bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-2xl text-transparent sm:text-4xl">
                             Enabling Creativity
                         </h1>
                         <div className="font-body text-[16] opacity-50">
@@ -75,9 +75,67 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="relative bottom-16 -z-10 h-28 w-full self-center bg-gradient-to-r from-amber-500 via-orange-500 via-30% to-[#ff2d68] blur-[32px]" />
+                <Image
+                    className="my-36 animate-[spin_30s_linear_infinite] self-center"
+                    src={humanIntelligence}
+                    alt="Human intelligence and some sparks of AGI only"
+                    width={140}
+                    height={140}
+                />
+                <div className="flex w-full flex-col gap-6 lg:flex-row">
+                    <div className="flex flex-col gap-8 rounded-lg bg-zinc-50 p-5 outline outline-1 outline-zinc-100 sm:gap-10 sm:p-8">
+                        <h1 className="bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-2xl text-transparent sm:text-4xl">
+                            Call for Papers
+                        </h1>
+                        <div className="font-body text-base opacity-50 lg:h-32">
+                            India HCI 2023 is the 14th edition of the
+                            international conference series on Human-Computer
+                            Interaction (HCI). Papers are the main medium for
+                            conveying new research results at India HCI. We seek
+                            submissions that push the boundaries of human
+                            knowledge and that describe original, unpublished
+                            work in the field of HCI.
+                        </div>
+                        <Link
+                            className="flex h-16 w-min flex-row items-center justify-center whitespace-nowrap rounded-2xl px-8 text-sm font-semibold uppercase text-zinc-900 outline outline-2 -outline-offset-2 outline-zinc-900 sm:h-[4.5rem]"
+                            href="/"
+                        >
+                            Read More
+                        </Link>
+                    </div>
+                    <div className="flex flex-col gap-8 rounded-lg bg-zinc-50 p-5 outline outline-1 outline-zinc-100 sm:gap-10 sm:p-8">
+                        <h1 className="bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-2xl text-transparent sm:text-4xl">
+                            Call for Posters
+                        </h1>
+                        <div className="font-body text-base opacity-50 lg:h-32">
+                            Posters and Demonstrations at India HCI 2023 will
+                            showcase your novel implementations, interaction
+                            designs, concepts, techniques, devices or systems to
+                            a wider audience at the conference and provide
+                            opportunities to interact with each other. We
+                            welcome posters, demonstrations & interactivity
+                            prototypes from students and researchers in all
+                            stages of their careers.
+                        </div>
+                        <Link
+                            className="flex h-16 w-min flex-row items-center justify-center whitespace-nowrap rounded-2xl px-8 text-sm font-semibold uppercase text-zinc-900 outline outline-2 -outline-offset-2 outline-zinc-900 sm:h-[4.5rem]"
+                            href="/"
+                        >
+                            Read More
+                        </Link>
+                    </div>
+                </div>
+            </main>
+            <div className="w-screen overflow-x-clip">
+                <div className="relative -z-10 -ml-12 mt-48 h-60 w-[110%] self-center bg-gradient-to-r from-amber-500 via-orange-500 via-30% to-[#ff2d68] blur-[100px] lg:h-80" />
+                <Marquee className="relative z-20 -mt-80 h-16 overflow-hidden font-heading text-6xl text-zinc-900/30 mix-blend-overlay lg:-mt-40 lg:h-28 lg:text-[100px]">
+                    Enabling&nbsp;Creativity&nbsp;Enabling&nbsp;Creativity&nbsp;Enabling&nbsp;Creativity&nbsp;
+                </Marquee>
+            </div>
+            <div className="sticky bottom-4 z-10 h-28 w-full self-center bg-gradient-to-r from-amber-500 via-orange-500 via-30% to-[#ff2d68] blur-[32px] lg:container lg:absolute lg:top-[700px]" />
+            <div className="container sticky bottom-0 z-20 self-center p-5 lg:absolute lg:top-[720px]">
                 <a
-                    className="relative bottom-32 flex h-[4.5rem] flex-row items-center justify-start gap-8 rounded-2xl bg-zinc-800 px-8 text-sm font-semibold uppercase text-white shadow-xl"
+                    className="flex h-[4.5rem] flex-row items-center justify-start gap-8 rounded-2xl bg-zinc-800 px-8 text-sm font-semibold uppercase text-white shadow-xl"
                     href="https://account.indiahci.org"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -93,61 +151,7 @@ export default function Home() {
                         />
                     </div>
                 </a>
-                <Image
-                    className="animate-[spin_30s_linear_infinite] self-center"
-                    src={humanIntelligence}
-                    alt="Human intelligence and some sparks of AGI only"
-                    width={140}
-                    height={140}
-                />
-                <div className="mt-36 flex w-full flex-row gap-6">
-                    <div className="flex flex-col gap-10 rounded-lg bg-zinc-50 p-8 outline outline-1 outline-zinc-100">
-                        <h1 className="relative bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-4xl text-transparent">
-                            Call for Papers
-                        </h1>
-                        <div className="h-32 font-body text-[16] opacity-50">
-                            India HCI 2023 is the 14th edition of the
-                            international conference series on Human-Computer
-                            Interaction (HCI). Papers are the main medium for
-                            conveying new research results at India HCI. We seek
-                            submissions that push the boundaries of human
-                            knowledge and that describe original, unpublished
-                            work in the field of HCI.
-                        </div>
-                        <Link
-                            className="flex h-[4.5rem] w-min flex-row items-center justify-center whitespace-nowrap rounded-2xl px-8 text-sm font-semibold uppercase text-zinc-900 outline outline-2 -outline-offset-2 outline-zinc-900"
-                            href="/"
-                        >
-                            Read More
-                        </Link>
-                    </div>
-                    <div className="flex flex-col gap-10 rounded-lg bg-zinc-50 p-8 outline outline-1 outline-zinc-100">
-                        <h1 className="bg-gradient-to-br from-amber-500 via-orange-500 via-30% to-[#FF0048] bg-clip-text font-heading text-4xl text-transparent">
-                            Call for Posters
-                        </h1>
-                        <div className="h-32 font-body text-[16] opacity-50">
-                            Posters and Demonstrations at India HCI 2023 will
-                            showcase your novel implementations, interaction
-                            designs, concepts, techniques, devices or systems to
-                            a wider audience at the conference and provide
-                            opportunities to interact with each other. We
-                            welcome posters, demonstrations & interactivity
-                            prototypes from students and researchers in all
-                            stages of their careers.
-                        </div>
-                        <Link
-                            className="flex h-[4.5rem] w-min flex-row items-center justify-center whitespace-nowrap rounded-2xl px-8 text-sm font-semibold uppercase text-zinc-900 outline outline-2 -outline-offset-2 outline-zinc-900"
-                            href="/"
-                        >
-                            Read More
-                        </Link>
-                    </div>
-                </div>
-            </main>
-            <div className="relative -bottom-16 -z-10 mt-48 h-60 w-[110%] self-center bg-gradient-to-r from-amber-500 via-orange-500 via-30% to-[#ff2d68] blur-[100px]" />
-            <Marquee className="-mt-52 font-heading text-[100px] text-zinc-900/30 mix-blend-overlay">
-                Enabling&nbsp;Creativity&nbsp;Enabling&nbsp;Creativity&nbsp;Enabling&nbsp;Creativity&nbsp;
-            </Marquee>
+            </div>
         </div>
     );
 }
